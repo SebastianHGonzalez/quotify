a9a80c7fd0f58b0cf186ef0d2471e062f1d38dc4
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2018-10-23 19:23:21 -0300

    Added quotation service test

---
+++ packages/currencify-service/src/QuotationService/QuotationService.test.ts
@@ -0,0 +1,43 @@
+import { DateTime } from 'luxon';
+
+import { QuotationService } from './QuotationService';
+import { USDARGQuotationStrategy } from './USDARGQuotationStrategy';
+import { Quotation } from './Quotation';
+
+describe(QuotationService, () => {
+    describe(USDARGQuotationStrategy, () => {
+
+        let expectedQuotation;
+        let strategy;
+        let quotationService;
+        let httpMock;
+        let usdargQuotationServiceResponse;
+
+        beforeEach(() => {
+            usdargQuotationServiceResponse = { data: ["35.800", "37.800", "Actualizada al 23/10/2018 15:00"] };
+            expectedQuotation = new Quotation(
+                'USD',
+                'ARG',
+                35.8,
+                37.8,
+                DateTime.fromFormat(
+                    usdargQuotationServiceResponse.data[2],
+                    "'Actualizada al' dd/LL/yyyy HH:mm"
+                ).toJSDate()
+            );
+
+            httpMock = {
+                get: jest.fn().mockResolvedValue(usdargQuotationServiceResponse)
+            };
+            
+            strategy = new USDARGQuotationStrategy(httpMock);
+            quotationService = new QuotationService(strategy);
+        });
+
+        it('retrieves cotization for usd', () => {
+            return quotationService.getQuotation().then((actualQuotation: Quotation) => {
+                expect(actualQuotation).toEqual(expectedQuotation);
+            });
+        });
+    });
+});
