b36db1b8abda6fa743ea4c7e01a9ba23004e5f84
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2018-10-26 00:27:48 -0300

    Fix: dependencies
    Fix: dependencies

--- packages/quotation-web-service/src/Handlers/USDARGQuotationServiceHandler.ts
+++ packages/quotation-web-service/src/Handlers/USDARGQuotationServiceHandler.ts
@@ -1,14 +1,5 @@
-import Axios from "axios";
-
-import { config } from "../config";
-
-import { QuotationService, USDARGQuotationStrategy, Quotation } from "@sebastianhgonzalez/quotation-service";
-
-
-export function USDARGQuotationServiceHandler(req: any, res: any) {
-    const http = Axios.create({ baseURL: config.quotationBaseUrl });
-    const quotationStrategy = new USDARGQuotationStrategy(http);
-    const quotationService = new QuotationService(quotationStrategy);
-
-    quotationService.getQuotation().then((quotation: Quotation) => res.json(quotation));
+export function USDARGQuotationServiceHandler(quotationService: any) {
+    return function (req: any, res: any) {
+        quotationService.getQuotation().then((quotation: any) => res.json(quotation));
+    };
 }
--- packages/quotation-web-service/src/startServer.ts
+++ packages/quotation-web-service/src/startServer.ts
@@ -1,4 +1,7 @@
+import Axios from "axios";
 import express from "express";
+
+import { QuotationService, USDARGQuotationStrategy } from "@sebastianhgonzalez/quotation-service";
 
 import { config } from "./config";
 
@@ -7,10 +10,13 @@ import {
     notImplementedHandler,
 } from "./Handlers";
 
+const http = Axios.create({ baseURL: config.quotationBaseUrl });
+const quotationStrategy = new USDARGQuotationStrategy(http);
+const quotationService = new QuotationService(quotationStrategy);
 
 const app = express();
 
-app.get("/Dolar", USDARGQuotationServiceHandler);
+app.get("/Dolar", USDARGQuotationServiceHandler(quotationService));
 app.get("/Pesos", notImplementedHandler);
 app.get("/Real", notImplementedHandler);
 
