05278c6fbe2a1c39d2e09dccb934a5fb833ee933
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2018-10-30 20:19:46 -0300

    Added quotations client interface

---
+++ packages/quotation-react-client/src/api/IQuotationsClient.ts
@@ -0,0 +1,9 @@
+import { Currency, Quotation } from '@sebastianhgonzalez/quotation-service';
+
+export type IQuotations = {
+  [currency in Currency]?: Quotation[];
+}
+
+export interface IQuotationsClient {
+  getQuotations(currencies: Currency[]): Promise<IQuotations>;
+}
