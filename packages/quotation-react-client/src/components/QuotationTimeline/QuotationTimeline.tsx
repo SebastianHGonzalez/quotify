d5ad65d4fd6966591593d8e08c6cbe34f63c0cf0
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2018-11-02 00:07:49 -0300

    Refactor: moved fetching quotation to individual timelines

--- packages/quotation-react-client/src/components/QuotationTimeline/QuotationTimeline.tsx
+++ packages/quotation-react-client/src/components/QuotationTimeline/QuotationTimeline.tsx
@@ -1,22 +1,94 @@
 import * as React from "react";
 
 import { List, ListItem } from "@material-ui/core";
-import { Quotation } from "@sebastianhgonzalez/quotation-service";
-import { QuotationCard } from "../QuotationCard";
+import { Currency, Quotation } from "@sebastianhgonzalez/quotation-service";
+import { IQuotationsClient } from "src/api";
+import { QuotationCard, QuotationErrorCard } from "../QuotationCard";
 
 interface IQuotationTimelineProps extends React.Props<any> {
-  currency: string;
+  quotationClient: IQuotationsClient;
+  maxQuotations?: number;
+  autoFetchInterval?: number;
+  currency: Currency;
+}
+
+interface IQuotationTimelineState {
+  autoFetchInterval: number;
+  error: any;
   quotations: Quotation[];
 }
 
-export function QuotationTimeline(props: IQuotationTimelineProps) {
-  return (
-    <List>
-      {props.quotations.map((quotation, index) => (
-        <ListItem key={index}>
-          <QuotationCard quotation={quotation} />
+export class QuotationTimeline extends React.Component<
+  IQuotationTimelineProps,
+  IQuotationTimelineState
+> {
+  constructor(props: IQuotationTimelineProps) {
+    super(props);
+
+    this.state = {
+      autoFetchInterval: props.autoFetchInterval || 0,
+      error: null,
+      quotations: []
+    };
+  }
+
+  public componentDidMount() {
+    this.autoFetchQuotations();
+  }
+
+  public autoFetchQuotations() {
+    this.fetchQuotations();
+    if (this.state.autoFetchInterval) {
+      setTimeout(
+        () => this.autoFetchQuotations(),
+        this.state.autoFetchInterval
+      );
+    }
+  }
+
+  public componentWillUnmount() {
+    this.setState({ autoFetchInterval: 0 });
+  }
+
+  public showError(error: any) {
+    this.setState({error, autoFetchInterval: 0})
+  }
+
+  public fetchQuotations() {
+    const currency = this.props.currency;
+    this.props.quotationClient
+      .getQuotation(currency)
+      .then((quotation: Quotation) => {
+        if (quotation) {
+          this.addQuotation(quotation);
+        }
+      })
+      .catch(err => {
+        this.showError(err);
+      });
+  }
+
+  public addQuotation(quotation: Quotation) {
+    const quotations: Quotation[] = [quotation, ...this.state.quotations];
+          this.setState({
+            quotations
+          });
+  }
+
+  public render() {
+    return (
+      <List>
+        {this.state.error && (
+        <ListItem>
+          <QuotationErrorCard currency={this.props.currency} />
         </ListItem>
-      ))}
-    </List>
-  );
+        )}
+        {this.state.quotations.map((quotation, index) => (
+          <ListItem key={index}>
+            <QuotationCard quotation={quotation} />
+          </ListItem>
+        ))}
+      </List>
+    );
+  }
 }
--- packages/quotation-react-client/src/components/QuotationTimelines/QuotationTimelines.tsx
+++ packages/quotation-react-client/src/components/QuotationTimelines/QuotationTimelines.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 
 import { Grid, GridList, withStyles } from "@material-ui/core";
 import { Currency } from "@sebastianhgonzalez/quotation-service";
-import { IQuotations, IQuotationsClient } from "src/api";
+import { IQuotationsClient } from "src/api";
 import { QuotationTimeline } from "src/components/QuotationTimeline";
 
 interface IQuotationTimelinesProps extends React.Props<any> {
@@ -12,71 +12,23 @@ interface IQuotationTimelinesProps extends React.Props<any> {
   currencies: Currency[];
 }
 
-interface IQuotationTimelinesState {
-  quotations: IQuotations;
-}
-
-class UnStyledQuotationTimelines extends React.Component<
-  IQuotationTimelinesProps,
-  IQuotationTimelinesState
-> {
-  constructor(props: IQuotationTimelinesProps) {
-    super(props);
-
-    this.state = {
-      quotations: {}
-    };
-
-    this.addQuotations = this.addQuotations.bind(this);
-    this.fetchQuotations = this.fetchQuotations.bind(this);
-    this.autoFetchQuotations = this.autoFetchQuotations.bind(this);
-  }
-
-  public componentDidMount() {
-    this.autoFetchQuotations();
-  }
-
-  public autoFetchQuotations() {
-    this.fetchQuotations();
-    if (this.props.autoFetchInterval) {
-      setTimeout(this.autoFetchQuotations, this.props.autoFetchInterval);
-    }
-  }
-
-  public fetchQuotations() {
-    this.props.quotationClient
-      .getQuotations(this.props.currencies)
-      .then(this.addQuotations);
-  }
-
-  public addQuotations(newQuotations: IQuotations) {
-    const quotations = { ...this.state.quotations };
-
-    Object.keys(newQuotations).forEach(currency => {
-      quotations[currency] = [
-        newQuotations[currency],
-        ...(this.state.quotations[currency] || [])
-      ].slice(0, this.props.maxQuotations);
-    });
-
-    this.setState({ quotations });
-  }
-
-  public render() {
+function UnStyledQuotationTimelines(props: IQuotationTimelinesProps)
+{
     return (
-      <GridList className={(this.props as any).classes.gridList}>
-        {Object.keys(this.state.quotations).map(currency => (
+      <GridList className={(props as any).classes.gridList}>
+        {props.currencies.map(currency => (
           <Grid item={true} key={currency}>
             <QuotationTimeline
               currency={currency}
-              quotations={this.state.quotations[currency]}
+              quotationClient={props.quotationClient}
+              maxQuotations={props.maxQuotations}
+              autoFetchInterval={props.autoFetchInterval}
             />
           </Grid>
         ))}
       </GridList>
     );
   }
-}
 
 const styles: any = (theme: any) => ({
   gridList: {
